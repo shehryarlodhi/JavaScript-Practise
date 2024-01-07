1dde015d3a74e0fe359f2e1ef86d6892ed273703
shehryarlodhi <lodhishehryarkhan@gmail.com>
2024-01-07 14:26:10 +0500

    counter App completed

---
+++ Counter-app/ReadMe.md
@@ -0,0 +1,28 @@
+JavaScript Counter App
+This is a simple JavaScript counter application designed for beginners as a small project to practice basic HTML, CSS, and JavaScript skills.
+
+Features
+Increment Counter: Click the "INCREMENT" button to increase the count by one.
+Save Counter: Save the current count value.
+View Previous Entries: Display a list of previously saved count values.
+Usage
+Clone or download the repository.
+
+Open the index.html file in a web browser.
+
+Click the "INCREMENT" button to increase the count.
+
+Click the "Save" button to store the current count.
+
+View the list of previous entries displayed below the counter.
+
+Technologies Used
+HTML: Structure
+CSS: Styling
+JavaScript: Functionality
+Preview
+
+Contributors
+Your Name
+License
+This project is licensed under the MIT License.
--- Counter-app/index.js
+++ Counter-app/index.js
@@ -1,14 +1,44 @@
-// var count = 0;
-// let countEl = document.getElementById("count-el");
-// function increment() {
-//     count++;
-//     console.log(count);
-//     countEl.innerText = count;
-// }
+var count = 0;
+let countEl = document.getElementById("count-el");
+function increment() {
+    count++;
+    console.log(count);
+    countEl.textContent = count;
+}
 
-// function save() {
-// console.log(count);
-// }
+
+
+let saveEl = document.getElementById("save-el");
+
+function save() {
+    countText = count + " - ";
+    saveEl.textContent +=  countText;
+    count = 0;
+    countEl.textContent = count;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // let n="Shehryar";
 // let greeting = "Hello! my name is ";
 // let myGreetings = greeting + n;
@@ -22,11 +52,11 @@
 // console.log("4" - 2);   
 // console.log("4" * 2);
 
-headingText = "Shehryar's First Website";
-myName = "Shehryar";
-greeting = `Hey! my name is ${myName}, welcome to my website!`;
-heading.innerText = headingText;
-para.innerText = greeting;
+// headingText = "Shehryar's First Website";
+// myName = "Shehryar";
+// greeting = `Hey! my name is ${myName}, welcome to my website!`;
+// heading.innerText = headingText;
+// para.innerText = greeting;
 
 
-para.innerText += " 👋🏻 ";
+// para.innerText += " 👋🏻 ";
