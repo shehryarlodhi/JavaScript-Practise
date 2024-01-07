893a133a649b8606edeef3495330313d7246eec7
shehryarlodhi <lodhishehryarkhan@gmail.com>
2024-01-07 12:56:04 +0500

    strings

--- Counter-app/index.js
+++ Counter-app/index.js
@@ -1,11 +1,32 @@
-var count = 0; 
-let countEl = document.getElementById("count-el");
-function increment() {
-    count++;
-    console.log(count);
-    countEl.innerText = count;
-}
+// var count = 0;
+// let countEl = document.getElementById("count-el");
+// function increment() {
+//     count++;
+//     console.log(count);
+//     countEl.innerText = count;
+// }
 
-function save() {
-console.log(count);
-}
+// function save() {
+// console.log(count);
+// }
+// let n="Shehryar";
+// let greeting = "Hello! my name is ";
+// let myGreetings = greeting + n;
+
+// console.log(myGreetings);
+// console.log(4 + 5);
+// console.log("4 + 5");
+// console.log("4" + "5");
+// console.log(4 + "5");
+// console.log("4" + 5);
+// console.log("4" - 2);   
+// console.log("4" * 2);
+
+headingText = "Shehryar's First Website";
+myName = "Shehryar";
+greeting = `Hey! my name is ${myName}, welcome to my website!`;
+heading.innerText = headingText;
+para.innerText = greeting;
+
+
+para.innerText += " 👋🏻 ";
