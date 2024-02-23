99af4bbbcc05df43a5bc276c034708fd9b02caee
shehryarlodhi <lodhishehryarkhan@gmail.com>
2024-02-23 11:21:20 +0500

    almost done

---
+++ Chrome-Extension/script.js
@@ -0,0 +1,22 @@
+const inputBtn = document.getElementById('input-btn');
+const inputEl = document.getElementById('input-el');
+const unorderedEl = document.getElementById('unordered-el');
+let myLeads = [];
+
+inputBtn.addEventListener('click', function () { 
+    myLeads.push(inputEl.value);
+    inputEl.value = "";
+    renderLeads();
+});
+
+function renderLeads() {
+    
+    let listItems = "";
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems +=`<li>
+                         <a target = _blank href='${myLeads[i]}'>  ${myLeads[i]}  </a>
+                    </li>`;
+    }
+    unorderedEl.innerHTML = listItems;
+
+}
